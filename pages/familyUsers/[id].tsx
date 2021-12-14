0442fb63277691b9703ffeab051249a30ac9e28e
Hoang Doan <hoang_doan@datahouse.com>
2021-12-14 09:43:23 +0700

    add ID family page

--- pages/familyUsers/[id].tsx
+++ pages/familyUsers/[id].tsx
@@ -4,6 +4,9 @@ import parse from 'html-react-parser';
 import { getEmailById, getEmails } from "../../lib/emails";
 import { Email } from "../emails";
 import { Fragment } from "react";
+import axios from "axios";
+import { API_URL } from "../../auth";
+import { FamilyUser } from "../familyUsers";
 
 
 interface Path {
@@ -12,22 +15,33 @@ interface Path {
   }
 }
 
+const parseIdFromHref = (href: string) => {
+  const words = href.split('/');
+  // Array ["http:", "", "54.255.38.53:7111", "api", "familyUsers", "1"]
+  return words[words.length - 1];
+}
 
 
-// export async function getStaticPaths() {
-//   const emails = getEmails();
-//   if (Array.isArray(emails)) {
-//     const paths = emails.map<Path>((item: Email) => {
-//       return { params: { id: item.filename } }
-//     })
-//     console.log("🚀 ~ file: [id].tsx ~ line 17 ~ getStaticPaths ~ paths", paths)
-//     // Return a list of possible value for id
-//     return {
-//       paths,
-//       fallback: false
-//     }
-//   }
-// }
+export async function getStaticPaths() {
+  try {
+    const res = await axios.get(`${API_URL}/api/familyUsers`);
+    const familyUsers: FamilyUser[] = res.data?.items || [];
+    const paths = familyUsers.map<Path>((user: FamilyUser) => {
+      return { params: { id: parseIdFromHref(user._href) } }
+    });
+    // Return a list of possible value for id
+    return {
+      paths,
+      fallback: false
+    }
+  } catch (error) {
+    console.log("🚀 ~ file: [id].tsx ~ line 24 ~ getStaticPaths ~ error", error)
+    return {
+      paths: [],
+      fallback: false
+    }
+  }
+}
 
 // Get external data from the file system, API, DB, etc.
 export async function getStaticProps({ params }: Path) {
